7e9ee1b09c03e95a6ab7cd4f74b7179eb77eed68
Paul Ngisiro <jngisiro@gmail.com>
2020-04-10 00:59:53 +0300

    Fix: parsing dollars in flight

--- src/estimator.js
+++ src/estimator.js
@@ -106,20 +106,20 @@ const covid19ImpactEstimator = (data) => {
   return response;
 };
 
-const data = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 4,
-    avgDailyIncomePopulation: 0.73
-  },
-  periodType: 'months',
-  timeToElapse: 38,
-  reportedCases: 2747,
-  population: 92931687,
-  totalHospitalBeds: 678874
-};
+// const data = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 4,
+//     avgDailyIncomePopulation: 0.73
+//   },
+//   periodType: 'months',
+//   timeToElapse: 38,
+//   reportedCases: 2747,
+//   population: 92931687,
+//   totalHospitalBeds: 678874
+// };
 
-console.log(covid19ImpactEstimator(data));
+// console.log(covid19ImpactEstimator(data));
 
-// module.exports = covid19ImpactEstimator;
+module.exports = covid19ImpactEstimator;
