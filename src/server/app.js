f1b5413aee4d690a0639aebce3195b98a3979825
Paul Ngisiro <jngisiro@gmail.com>
2020-04-10 16:38:28 +0300

    Feat: CORS for API

--- src/server/app.js
+++ src/server/app.js
@@ -2,18 +2,26 @@ const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
 
 // create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' }
+);
 
 // setup the logger
 app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(express.json());
+
+//  configure cors
+app.use(cors());
+app.options('*', cors());
 
 app.use('/api/v1', routes);
 
